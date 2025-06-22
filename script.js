9e7752fa6880e140bc7ef7c362d49fa1ea6aa49f
Ubuntu <ubuntu@ip-172-31-43-73.eu-north-1.compute.internal>
2025-06-22 11:45:38 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Welcome to Abdul Ganiyu's Cloud Capstone Website!");
+  alert("Thank you for visiting my Capstone Project!");
+});
